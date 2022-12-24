67c4c89c5dfce95394424b2c243da726e44fd88a
horodeski <geovana.horodeski06@gmai.com>
2022-12-24 17:46:42 -0300

    gsh

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script>
+export default {};
+</script>
+<template>
+  <nav>
+    <div class="logo">
+      <h1>
+        HORODESKI<br />
+        <span> developer </span>
+      </h1>
+    </div>
+    <input type="checkbox" id="click" />
+    <label for="click" class="menu-btn">
+      <i class="fas fa-bars"></i>
+    </label>
+
+    <ul>
+      <li><a class="active" href="#">Home</a></li>
+      <li><a href="#">Sobre mim</a></li>
+      <li><a href="#">Projetos</a></li>
+    </ul>
+  </nav>
+  <div class="content">
+    <div>Responsive Navigation Menu Bar Design</div>
+    <div>using only HTML & CSS</div>
+  </div>
+</template>

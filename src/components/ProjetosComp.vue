e01038a080f3d5ff369f91e23dbb84a2392a132e
horodeski <geovana.horodeski06@gmail.com>
2023-02-22 15:00:33 -0300

    gsh

--- src/components/ProjetosComp.vue
+++ src/components/ProjetosComp.vue
@@ -9,7 +9,7 @@ export default {};
       <div>
         <h2>BETFREG</h2>
         <p>Landing page responsiva</p>
-        <div>
+        <div class="mais">
           <button>ver mais</button>
           <box-icon
             size="lg"
@@ -29,16 +29,16 @@ export default {};
       </div>
     </div>
     <div class="card">
-      <img src="/src/assets/img/betferg.png" alt="" />
+      <img src="/src/assets/img/markantonymovies.png" alt="" />
       <div>
-        <h2>BETFREG</h2>
-        <p>Landing page responsiva</p>
-        <div>
+        <h2>MAM</h2>
+        <p>Puxando api do TMDB com axios em VueJS</p>
+        <div class="mais">
           <button>ver mais</button>
           <box-icon
             size="lg"
             class="logo-skills"
-            name="html5"
+            name="vuejs"
             type="logo"
             color="#ffffff"
           ></box-icon>
@@ -53,23 +53,23 @@ export default {};
       </div>
     </div>
     <div class="card">
-      <img src="/src/assets/img/betferg.png" alt="" />
+      <img src="/src/assets/img/livrariadodudu.png" alt="" />
       <div>
-        <h2>BETFREG</h2>
-        <p>Landing page responsiva</p>
-        <div>
+        <h2>Livraria do DUDU</h2>
+        <p>Puxando dados do BackEnd em javascript puro com axios</p>
+        <div class="mais">
           <button>ver mais</button>
           <box-icon
             size="lg"
             class="logo-skills"
-            name="html5"
+            name="vuejs"
             type="logo"
             color="#ffffff"
           ></box-icon>
           <box-icon
             size="lg"
             class="logo-skills"
-            name="css3"
+            name="heroku"
             type="logo"
             color="#ffffff"
           ></box-icon>
@@ -77,47 +77,23 @@ export default {};
       </div>
     </div>
     <div class="card">
-      <img src="/src/assets/img/betferg.png" alt="" />
+      <img src="/src/assets/img/calculadora.png" alt="" />
       <div>
-        <h2>BETFREG</h2>
-        <p>Landing page responsiva</p>
-        <div>
+        <h2>Calculadora de gorjetas</h2>
+        <p>Calculadora de gorjetas em vue</p>
+        <div class="mais">
           <button>ver mais</button>
           <box-icon
             size="lg"
             class="logo-skills"
-            name="html5"
+            name="vuejs"
             type="logo"
             color="#ffffff"
           ></box-icon>
           <box-icon
             size="lg"
             class="logo-skills"
-            name="css3"
-            type="logo"
-            color="#ffffff"
-          ></box-icon>
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <img src="/src/assets/img/betferg.png" alt="" />
-      <div>
-        <h2>BETFREG</h2>
-        <p>Landing page responsiva</p>
-        <div>
-          <button>ver mais</button>
-          <box-icon
-            size="lg"
-            class="logo-skills"
-            name="html5"
-            type="logo"
-            color="#ffffff"
-          ></box-icon>
-          <box-icon
-            size="lg"
-            class="logo-skills"
-            name="css3"
+            name="javascript"
             type="logo"
             color="#ffffff"
           ></box-icon>

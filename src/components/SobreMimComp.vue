cec052b673180e82a4d443caf7eef1b0be307966
horodeski <geovana.horodeski06@gmail.com>
2023-04-16 00:16:24 -0300

    ajustes no sobre mim

--- src/components/SobreMimComp.vue
+++ src/components/SobreMimComp.vue
@@ -7,14 +7,66 @@ export default {};
     <div class="eu-softSkills">
       <div class="eu">
         <img src="/src/assets/img/eu.jpg" alt="eu" />
-        <p>
-          Sou uma desenvolvedora <span>FullStack</span> apaixonada por criar
-          soluções web incríveis e intuitivas para os usuários.
-        </p>
+        <div>
+          <p>
+            <span>&lt;p&gt;</span>
+            Sou uma desenvolvedora <span>FullStack</span> apaixonada por criar
+            soluções web incríveis e intuitivas para os usuários.
+            <span>&lt;/p&gt;</span>
+          </p>
+          <div class="infos">
+            <div>
+              <div>
+                <h3 class="topico">NOME:</h3>
+                <h3>Geovana Sophia Horodeski</h3>
+              </div>
+              <div>
+                <h3 class="topico">NASCIMENTO:</h3>
+                <h3>3 de julho de 2006</h3>
+              </div>
+              <div>
+                <h3 class="topico">EMAIL:</h3>
+                <h3>geovana.horodeski06@gmail.com</h3>
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3 class="topico">BIO:</h3>
+                <h3>Gosto de ABBA, filmes e FrontEnd</h3>
+              </div>
+              <div class="eu-interesses">
+                <div>
+                  <h3 class="topico">Interesses</h3>
+                  <div>
+                    <box-icon
+                      size="lg"
+                      name="camera-movie"
+                      type="solid"
+                      color="#ffffff"
+                    ></box-icon>
+                    <box-icon
+                      size="lg"
+                      type="logo"
+                      name="css3"
+                      color="#ffffff"
+                    ></box-icon>
+                    <box-icon
+                      size="lg"
+                      type="logo"
+                      name="imdb"
+                      color="#ffffff"
+                    ></box-icon>
+                    <box-icon size="lg" name="music" color="#ffffff"></box-icon>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="soft-skills">
         <div class="card-skills">
-          <box-icon size="lg" color="#fff" name="cool"></box-icon>
+          <box-icon size="5em" color="#fff" name="cool"></box-icon>
           <div>
             <h3>Trabalho em equipe</h3>
             <p>
@@ -24,51 +76,24 @@ export default {};
           </div>
         </div>
         <div class="card-skills">
-          <box-icon size="lg" color="#fff" name="cool"></box-icon>
+          <box-icon size="5em" name="shocked" color="#ffffff"></box-icon>
           <div>
-            <h3>Trabalho em equipe</h3>
+            <h3>Resolução de problemas</h3>
             <p>
-              capacidade de colaborar com os outros, ser flexível e adaptar-se a
-              diferentes personalidades e estilos de trabalho
+              a habilidade de identificar e resolver problemas técnicos e de
+              negócios de forma eficaz.
             </p>
           </div>
         </div>
         <div class="card-skills">
-          <box-icon size="lg" color="#fff" name="cool"></box-icon>
+          <box-icon size="5em" name="wink-tongue" color="#ffffff"></box-icon>
           <div>
-            <h3>Trabalho em equipe</h3>
+            <h3>Criatividade</h3>
             <p>
-              capacidade de colaborar com os outros, ser flexível e adaptar-se a
-              diferentes personalidades e estilos de trabalho
+              a habilidade de pensar fora da caixa para encontrar soluções
+              inovadoras para problema.
             </p>
           </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="eu-interesses">
-      <div>
-        <h3>Interesses</h3>
-        <div>
-          <box-icon
-            size="lg"
-            name="camera-movie"
-            type="solid"
-            color="#ffffff"
-          ></box-icon>
-          <box-icon
-            size="lg"
-            type="logo"
-            name="css3"
-            color="#ffffff"
-          ></box-icon>
-          <box-icon
-            size="lg"
-            type="logo"
-            name="imdb"
-            color="#ffffff"
-          ></box-icon>
-          <box-icon size="lg" name="music" color="#ffffff"></box-icon>
         </div>
       </div>
     </div>

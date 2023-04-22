98101ece6c6ad0a6c920dadbaf346631e03e1673
horodeski <geovana.horodeski06@gmail.com>
2023-04-22 11:15:18 -0300

    arrumando formacao

--- src/components/FormacaoComp.vue
+++ src/components/FormacaoComp.vue
@@ -2,12 +2,13 @@
 export default {};
 </script>
 <template>
-  <h2>Formação</h2>
   <div class="conteudo">
-    <img src="/src/assets/img/if.png" alt="" />
-    <div>
+    <h2>Qualificação</h2>
+    <div class="ifc">
+      <img src="/src/assets/img/if.png" alt="" />
       <h3>Técnico em informática para internet (2021 - 2023)</h3>
       <p>Instituto Federal Catarinense - campus Araquari</p>
     </div>
   </div>
+  <hr id="two" />
 </template>
